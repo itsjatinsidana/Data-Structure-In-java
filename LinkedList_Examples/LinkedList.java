4a1d3171e77b17d7ab8824dfe83ab58e843c9dc3
jatin sidana <jatinsadana1998@gmail.com>
2023-07-27 12:05:20 +0530

    size of list

--- LinkedList_Examples/LinkedList.java
+++ LinkedList_Examples/LinkedList.java
@@ -1,55 +1,68 @@
 package LinkedList_Examples;
 
 public class LinkedList {
-   Node start;
+    Node start;
 
-   void makeList(){
+    void makeList() {
 
-   Node nd1 = new Node(10);
-   Node nd2 = new Node(20);
-   Node nd3 = new Node(30);
-   Node nd4 = new Node(40);
-   Node nd5 = new Node(50);
-   
-   start = nd1;
-   nd1.next = nd2;
-   nd2.next = nd3;
-   nd3.next = nd4;
-   nd4.next = nd5;
+        Node nd1 = new Node(10);
+        Node nd2 = new Node(20);
+        Node nd3 = new Node(30);
+        Node nd4 = new Node(40);
+        Node nd5 = new Node(50);
 
-    
-   }
+        start = nd1;
+        nd1.next = nd2;
+        nd2.next = nd3;
+        nd3.next = nd4;
+        nd4.next = nd5;
 
-   void printList(){
-   Node t = start;
-   while (t!=null) {
-    System.out.print(t.val + "-->");
-    t=t.next;
-   }
-   System.out.println("x");
-
-   }
-    boolean isEmpty(){
-      if(start == null){
-        return true;
-      }
-      else {
-        return false;
-      }
     }
-       void add(int val){
-        if(isEmpty()){
+
+    void printList() {
+        Node t = start;
+        while (t != null) {
+            System.out.print(t.val + "-->");
+            t = t.next;
+        }
+        System.out.println("x");
+
+    }
+
+    boolean isEmpty() {
+        if (start == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void add(int val) {
+        if (isEmpty()) {
             Node nd = new Node(val);
             start = nd;
-        }
-        else{
-          Node  t = start;
-            while(t.next != null){
+        } else {
+            Node t = start;
+            while (t.next != null) {
                 t = t.next;
             }
             Node nd = new Node(val);
             t.next = nd;
         }
-       }
-    
+    }
+
+    int size() {
+
+        int count = 0;
+        Node t = start;
+        while (t != null) {
+
+            t = t.next;
+            count++;
+
+        }
+        return count;
+
+    }
+
 }
--- LinkedList_Examples/Testing_Linked_List.java
+++ LinkedList_Examples/Testing_Linked_List.java
@@ -3,13 +3,13 @@ package LinkedList_Examples;
 public class Testing_Linked_List {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.makeList();
-        ll.printList();
+        
         ll.add(50);
         ll.add(50);
         ll.add(50);
         ll.add(50);
         ll.add(50);
         ll.printList();
+        System.out.println("size of list is " + ll.size());
     }
 }
