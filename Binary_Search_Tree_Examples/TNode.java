327d50d4d15505706368deff4f93d7e1fff034fe
jatin sidana <jatinsadana1998@gmail.com>
2023-07-29 22:09:23 +0530

    check tree is empty or not

---
+++ Binary_Search_Tree_Examples/TNode.java
@@ -0,0 +1,16 @@
+package Binary_Search_Tree_Examples;
+
+public class TNode {
+   
+        int val;
+        TNode LEFT;
+        TNode RIGHT;
+        
+        public TNode(int val){
+            this.val=  val;
+            this.LEFT= null;
+            this.RIGHT = null;
+        }
+        
+    
+}
