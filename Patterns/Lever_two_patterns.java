d7393a0f03ed86bfcf9b917f14dc806a9b2be4f9
jatin sidana <jatinsadana1998@gmail.com>
2023-08-01 13:02:43 +0530

    level 3 patterns

--- Patterns/Lever_two_patterns.java
+++ Patterns/Lever_two_patterns.java
@@ -6,11 +6,23 @@ public class Lever_two_patterns {
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= 5 - i; j++) {
 
-                System.out.print("");
+                System.out.print("  ");
             }
 
             for (int k = 1; k <=2* i - 1; k++) {
-                System.out.print("*");
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+         for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+
+                System.out.print("  ");
+            }
+
+            for (int k = 1; k <= i ; k++) {
+                System.out.print(k + " ");
             }
             System.out.println();
         }
---
+++ Patterns/Pattern_Level_three.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class Pattern_Level_three {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+
+                System.out.print("  ");
+            }
+
+            for (int k = 1; k <=i; k++) {
+                System.out.print(k + " ");
+            }
+            for(int l = i-1;l>=1;l--){
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+    }
+}
