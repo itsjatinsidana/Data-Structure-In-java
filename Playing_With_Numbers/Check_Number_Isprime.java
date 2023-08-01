6aa048c31fb3a745f6f8fbe13b47a12774884d78
jatin sidana <jatinsadana1998@gmail.com>
2023-08-01 14:00:15 +0530

    check number is prime or not

---
+++ Playing_With_Numbers/Check_Number_Isprime.java
@@ -0,0 +1,28 @@
+package Playing_With_Numbers;
+
+import java.util.*;
+
+public class Check_Number_Isprime {
+    public static void main(String[] args) {
+         int n = 17;
+        System.out.println( n + " is " + isPrime(n));
+
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1 || n == 0) {
+            return false;
+        } else {
+            boolean flag = true;
+           
+            for (int i = 2; i<=Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    flag = false;
+                   
+                }
+            }
+            return flag;
+           
+        }
+    }
+}
