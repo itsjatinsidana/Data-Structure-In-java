710bbf62db2d26cb9cb2d5edf89637b9180c289e
jatin sidana <jatinsadana1998@gmail.com>
2023-08-01 16:07:32 +0530

    twin prime

---
+++ Playing_With_Numbers/Twin_Prime.java
@@ -0,0 +1,30 @@
+package Playing_With_Numbers;
+
+public class Twin_Prime {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100 ; i ++){
+
+          if(isPrime(i) && isPrime(i+2)){
+System.out.println(i+" ," +(i+2));
+
+          }
+
+         
+        }
+        
+    }
+    static boolean isPrime(int n) {
+        boolean flag = true;
+        if (n == 1 || n == 0) {
+            return false;
+        } else {
+
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+    }
+}
