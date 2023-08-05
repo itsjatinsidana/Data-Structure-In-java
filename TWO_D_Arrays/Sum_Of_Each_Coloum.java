5a9fbca0bdc20d784307d99b1006fa93b11dddcd
jatin sidana <jatinsadana1998@gmail.com>
2023-08-05 20:10:58 +0530

    sum of each coloum

--- TWO_D_Arrays/Sum_Of_Each_Coloum.java
+++ TWO_D_Arrays/Sum_Of_Each_Coloum.java
@@ -1,19 +1,19 @@
 package TWO_D_Arrays;
 
-public class Sum_Of_Each_Row {
+public class Sum_Of_Each_Coloum {
     public static void main(String[] args) {
         int a[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        sumOfEachRow(a);
+        sumOfEachColoum(a);
 
     }
 
-    static void sumOfEachRow(int a[][]) {
+    static void sumOfEachColoum(int a[][]) {
         int i, j, m = a.length, n = a[0].length;
         int sum = 0;
         for (i = 0; i < m; i++) {
             sum = 0;
             for (j = 0; j < n; j++) {
-                sum = sum + a[i][j];
+                sum = sum + a[j][i];
             }
             System.out.println("sum of " + i + " is " + sum);
 
