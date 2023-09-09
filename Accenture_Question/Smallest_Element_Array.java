4b5cbbc13d15d43b2118eb8fb92a3b647898a406
jatin sidana <jatinsadana1998@gmail.com>
2023-09-09 23:06:01 +0530

    minimum array variation 2

--- Accenture_Question/Smallest_Element_Array.java
+++ Accenture_Question/Smallest_Element_Array.java
@@ -2,19 +2,20 @@ package Accenture_Question;
 
 public class Smallest_Element_Array {
     public static void main(String[] args) {
-        int a[] = {5,10,20,2,6,1};
-    
+        int a[] = { 5, 10, 20, 2, 6, 1 };
+
         smallestElement(a);
 
     }
-    static void smallestElement(int a[]){
+
+    static void smallestElement(int a[]) {
         int i;
         int min = a[0];
-        for(i=0;i<a.length;i++){
-            if(min>=a[i]){
-min = a[i];
+        for (i = 0; i < a.length; i++) {
+            if (min >= a[i]) {
+                min = a[i];
             }
-            
+
         }
         System.out.println("mininmum element in array is " + min);
     }
---
+++ Accenture_Question/Smallest_Element_Varriation_2.java
@@ -0,0 +1,16 @@
+package Accenture_Question;
+
+import java.util.Arrays;
+
+public class Smallest_Element_Varriation_2 {
+    public static void main(String[] args) {
+        int a[] = {5,10,20,2,6};
+    
+        smallestElement(a);
+
+    }
+    static void smallestElement(int a[]){
+        Arrays.sort(a);
+        System.out.println(a[0]);
+    }
+}
