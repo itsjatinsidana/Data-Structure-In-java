62a4a365dea5e9ef810b3899bdd3b569bc464faa
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2023-10-05 17:11:52 +0530

    changes

--- Accenture_Question/rev_char.java
+++ Accenture_Question/rev_char.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 public class rev_char {
     public static void main(String[] args) {
         String input = "Hello world";
-        System.out.println(reverse(input));
+        System.out.print(reverse(input));
     }
 
     static String reverse(String input) {
@@ -14,13 +14,13 @@ public class rev_char {
         int n = st.countTokens();
         for (int i = 0; i <= n; i++) {
             String ans = st.nextToken();
-            System.out.println(ans);
+           
             StringBuffer sb = new StringBuffer(ans);
             sb = sb.reverse();
             System.out.println(sb);
-            output = output+sb;
+            output = output + sb;
         }
-        System.out.println(output);
+      
         return output;
     }
 
