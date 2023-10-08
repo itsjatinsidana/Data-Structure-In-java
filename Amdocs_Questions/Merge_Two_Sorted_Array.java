5867c11a3b8459166c5d35976546e33b1b38fda1
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2023-10-08 13:32:11 +0530

    merge sorted array changes

--- Amdocs_Questions/Merge_Two_Sorted_Array.java
+++ Amdocs_Questions/Merge_Two_Sorted_Array.java
@@ -1,25 +1,42 @@
 package Amdocs_Questions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Merge_Two_Sorted_Array {
     public static void main(String[] args) {
-        String s = "hello world";
-        System.out.println(reverse(s));
+        int[] a = { 1, 4, 7 ,0,0,2};
+        int[] b = { 2,0, 3, 9 };
+        int[] c = merge(a, b);
+        System.out.println(Arrays.toString(c));
     }
 
-    static String reverse(String s) {
-        String rev = "";
-        StringTokenizer st = new StringTokenizer(s);
-        int n = st.countTokens();
-        for (int i = 0; i <= n; i++) {
-            String ans = st.nextToken();
-            StringBuffer sb = new StringBuffer(ans);
-            sb.reverse();
-           
-            rev = sb + rev;
+    static int[] merge(int[] a, int[] b) {
+
+        ArrayList<Integer> al = new ArrayList<>();
+       
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]!=0){
+            al.add(a[i]);
+            }
         }
-        return rev;
+        for (int i = 0; i < b.length; i++) {
+            if(b[i]!=0){
+            al.add(b[i]);
+            }
+        }
+        Collections.sort(al);
+        System.out.println(al);
+        int j = 0;
+        int[] c = new int[al.size()];
+        for (int p : al) {
+            c[j] = p;
+            j++;
+        }
+        return c;
 
     }
+
 }
