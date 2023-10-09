028e661683a73bdb86cd18b8e27e9520ffa1df17
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2023-10-09 17:55:06 +0530

    one test case clear

--- Amdocs_Questions/Print_prime_which_are_sum.java
+++ Amdocs_Questions/Print_prime_which_are_sum.java
@@ -1,21 +1,49 @@
 package Amdocs_Questions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Print_prime_which_are_sum {
     public static void main(String[] args) {
         int n = 10;
         System.out.println(isPrime(n));
+        int[] c = sumOfPrime(n);
+        System.out.println(Arrays.toString(c));
 
+    }
+
+    static int[] sumOfPrime(int n) {
         ArrayList<Integer> al = new ArrayList<>();
+        ArrayList<Integer> al2 = new ArrayList<>();
 
         for (int i = 0; i <= n; i++) {
             if (isPrime(i)) {
                 al.add(i);
-              }
+            }
 
         }
-        System.out.println(al);
+
+        int[] c = new int[al.size()];
+        int j = 0;
+        int sum = 0;
+        for (int p : al) {
+            c[j] = p;
+            j++;
+        }
+        for (int i = 0; i < c.length; i++) {
+            for (j = i+1; j < c.length; j++) {
+                if (c[i] + c[j] == n) {
+                    al2.add(c[i]);
+                    al2.add(c[j]);
+                }
+            }
+        }
+       int[] result = new int[al2.size()];
+       for(int i =0;i<result.length;i++){
+        result[i] = al2.get(i);
+       }
+        return result;
+         
 
     }
 
