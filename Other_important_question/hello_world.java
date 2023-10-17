46a8fded964b974a1cff6ff6f5caae6f6177c416
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2023-10-17 23:26:01 +0530

    easy code

---
+++ Other_important_question/hello_world.java
@@ -0,0 +1,9 @@
+package Other_important_question;
+
+public class hello_world {
+    public static void main(String[] args) {
+       String s ="hello world";
+    System.out.println(s);  
+    }
+   
+}
