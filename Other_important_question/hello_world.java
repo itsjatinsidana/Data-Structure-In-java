cef31ac95a30367598aaf126bbe58edfab4988c0
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2023-10-18 23:33:52 +0530

    changes in hello

--- Other_important_question/hello_world.java
+++ Other_important_question/hello_world.java
@@ -4,6 +4,9 @@ public class hello_world {
     public static void main(String[] args) {
        String s ="hello world";
     System.out.println(s);  
+    for(int i = 0;i<=s.length();i++){
+        System.out.println(s);
+    }
     }
    
 }
